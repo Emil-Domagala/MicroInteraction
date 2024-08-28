4da2aee52bcb0a21476044cceba284edf6b22c4f
EMIL DOMAGALA <emildomagala@MacBook-Air-EMIL-2.local>
2024-08-28 17:33:11 +0200

    working at 5th card

--- src/components/hero/animationsExamples/Animation5.js
+++ src/components/hero/animationsExamples/Animation5.js
@@ -1,8 +1,41 @@
-import Card from '../../UI/Card'
-import classes from './Animation5.module.scss'
+import Card from '../../UI/Card';
+import classes from './Animation5.module.scss';
+import AppleLogoSVG from '../../../assets/AppleLogoSVG';
+import ArrowSVG from '../../../assets/ArrowSVG';
+import ChevronSVG from '../../../assets/ChevronSVG';
 
-const Animation5=()=>{
-  return <Card bgc="bgc-black-brand" border="border-grey"></Card>;
-
-}
-export default Animation5
+const Animation5 = () => {
+  return (
+    <Card bgc="bgc-black-brand" border="border-grey">
+      <a href="/" className={`link-button ${classes.first}`}>
+        <div className={classes['apple-logo']}>
+          <AppleLogoSVG />
+        </div>
+        <p className={classes['text']}>Download for iOS</p>
+        <div className={classes['arrow']}>
+          <ArrowSVG />
+        </div>
+      </a>
+      <a href="/" className={`link-button rounded ${classes.second}`}>
+        <div className={classes['animation-wrapper']}>
+          <div className={classes['chevron']}>
+            <ChevronSVG />
+          </div>
+        </div>
+        Newsletter
+      </a>
+      <a href="/" className={`link-button rounded ${classes.third}`}>
+        Subscribe
+        <div className={classes['arrows-wrapper']}>
+          <div className={classes['top']}>
+            <ArrowSVG />
+          </div>
+          <div className={classes['bottom']}>
+            <ArrowSVG />
+          </div>
+        </div>
+      </a>
+    </Card>
+  );
+};
+export default Animation5;
