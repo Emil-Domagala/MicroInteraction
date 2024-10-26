80e011936d150ad8c0c427863b356340dded29fc
EMIL DOMAGALA <emildomagala@MacBook-Air-EMIL-2.local>
2024-10-26 21:12:51 +0200

    created favicon

--- src/App.js
+++ src/App.js
@@ -8,17 +8,16 @@ import Section5 from './components/sections/section5/Section5';
 import Footer from './components/footer/Footer';
 
 function App() {
-
   return (
     <>
       <Navigation />
       <Hero />
       <Section1 />
       <Section2 />
-      <Section3/>
-      <Section4/>
-      <Section5/>
-      <Footer/>
+      <Section3 />
+      <Section4 />
+      <Section5 />
+      <Footer />
     </>
   );
 }
